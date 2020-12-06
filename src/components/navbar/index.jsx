af4b1e40fa546c95519c16949cffdcb1b03876d1
Your Name <you@example.com>
2020-12-06 16:06:50 +0530

    yupp

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import { Button } from "../button";
+import { Logo } from "../logo";
+import { Marginer } from "../marginer";
+
+const NavbarContainer = styled.div`
+  width: 100%;
+  height: 50px;
+  padding: 0 2em;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const BrandContainer = styled.div``;
+
+const AccessibilityContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const LoginButton = styled(Button)`
+  background-color: transparent;
+  border: none;
+
+  &:hover {
+    background-color: transparent;
+    border: none;
+    color: rgb(212, 212, 212);
+  }
+`;
+
+export function Navbar(props) {
+  return (
+    <NavbarContainer>
+      <BrandContainer>
+        <Logo inline />
+      </BrandContainer>
+      <AccessibilityContainer>
+        
+        <Marginer direction="horizontal" margin="25px" />
+        <LoginButton large>Home</LoginButton>
+        <LoginButton large>About</LoginButton><LoginButton large>Contact</LoginButton>
+        <LoginButton large>Projects</LoginButton>
+      </AccessibilityContainer>
+    </NavbarContainer>
+  );
+}
